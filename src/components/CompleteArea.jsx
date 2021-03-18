28dd6e9dad3872a1367e98c54663643c4cddad91
yujiro-hirashima <75685573+yujiro-hirashima@users.noreply.github.com>
2021-03-18 14:30:54 +0900

    titleChange タイトルを変えました。

--- src/components/CompleteArea.jsx
+++ src/components/CompleteArea.jsx
@@ -1,20 +1,18 @@
-export const CompleteArea = (props)=>{
-  const {completeTodo,onClick} = props;
-  return(
+export const CompleteArea = (props) => {
+  const { completeTodo, onClick } = props;
+  return (
     <div className="complete-area">
-    <p className="title">完了のTODO</p>
-    <ul>
-      {completeTodo.map((todo, index) => {
-        return (
-          <div key="todo" className="list-row">
-            <li>{todo}</li>
-            <button onClick={() => onClick(index)}>戻す</button>
-          </div>
-    );
-  })}
-  </ul>
-  </div>
-
-
+      <p className="title">完了のTODOリスト</p>
+      <ul>
+        {completeTodo.map((todo, index) => {
+          return (
+            <div key="todo" className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClick(index)}>戻す</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
   );
-}
+};
